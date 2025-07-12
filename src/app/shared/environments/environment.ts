741ebf298cd41315a8150978331de8bd7773cb83
toàn trần <trandactoan2001@gmail.com>
2025-07-12 21:40:05 +0700

    Change url link

--- src/app/shared/environments/environment.ts
+++ src/app/shared/environments/environment.ts
@@ -1,6 +1,4 @@
 // src/environments/environment.ts
 export const environment = {
-  // base_url: 'https://leining361.duckdns.org/'
-  base_url: 'http://localhost:8080/',
-  zns_template_id: '459069'
+  base_url: 'https://leining361.duckdns.org/'
 };
